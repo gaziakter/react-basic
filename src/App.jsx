6484d4012bcba716b6fd229457852247ffbcbc38
Gazi Akter <gaziakterpro@gmail.com>
2025-05-30 11:35:29 +0600

    use css and components

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './Header'
+import About from './About'
+import Contact from './components/Contact/Contact'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +13,8 @@ function App() {
     <>
       <h1>Vite + React</h1>
       <Home></Home>
+      <About></About>
+      <Contact></Contact>
     </>
   )
 }
