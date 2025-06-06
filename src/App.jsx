146c69b8d32bbc97896c17f4d817db6b0e4a08a0
Gazi Akter <gaziakterpro@gmail.com>
2025-06-06 21:35:36 +0600

    add navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Navigation from './components/Navigation/Navigation'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Navigation></Navigation>
     </>
   )
 }
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import './Navigation.css'
 
 const Navigation = () => {
   return (
-    <div>Navigation</div>
+    <div>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Portfolio</a></li>
+            <li><a href="">Services</a></li>
+            <li><a href="">Blog</a></li>
+            <li><a href="">contact</a></li>
+        </ul>
+    </div>
   )
 }
 
