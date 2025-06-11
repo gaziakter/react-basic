cc58333ec13827dafbbbce8de37f2797601dc382
Gazi Akter <gaziakterpro@gmail.com>
2025-06-11 11:43:42 +0600

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Header from "./components/Header/Header"
 import Product from "./components/Products/Product"
 
 const App = () => {
 
   return (
     <div>
+      <Header></Header>
       <Product></Product>
     </div>
   )
