280cc392bbb848c242ed997096d91b6a94542dbe
Gazi Akter <gaziakterpro@gmail.com>
2025-06-27 10:14:37 +0600

    install react

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
-// App.jsx
-import { Routes, Route, Link } from 'react-router-dom'
-import Home from './components/Page/Home'
-import About from './components/Page/About'
-import Contact from './components/Page/Contact'
+import { useState } from 'react'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <div>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
-      </nav>
-      
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
     </div>
   )
 }
